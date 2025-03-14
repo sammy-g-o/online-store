3469d204524778ffa9082c78050d6dd7916e2a81
Giwa Abdulsamad <giwaabdulsamad0@gmail.com>
2025-03-14 21:53:45 +0000

    kept images in public and corrected the paths

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const deviceInfo = [
   {
-    image: "devices/DELL-XPS-9510.png",
+    image: "/devices/DELL-XPS-9510.jpg",
     name: "Dell XPS 9510",
     price: "950,000",
     rating: 4,
@@ -8,7 +8,7 @@ export const deviceInfo = [
     quantity:0
   },
   {
-    image: "devices/samsung-galaxy-s21.jpg",
+    image: "/devices/samsung-galaxy-s21.jpg",
     name: "Samsung Galaxy S21",
     price: "1,009,000",
     rating: 4,
@@ -16,7 +16,7 @@ export const deviceInfo = [
     quantity:0
   },
   {
-    image: "devices/iphone-14-pro-max.jpg",
+    image: "/devices/iphone-14-pro-max.jpg",
     name: "Iphone 14 Pro Max",
     price: "1,499,000",
     rating: 4,
@@ -24,7 +24,7 @@ export const deviceInfo = [
     quantity:0
   },
   {
-    image: "devices/samsung-galaxy-z-flip.png",
+    image: "/devices/samsung-galaxy-z-flip.jpg",
     name: "Samsung Galaxy Z Flip",
     price: "1,599,000",
     rating: 4,
@@ -32,7 +32,7 @@ export const deviceInfo = [
     quantity:0
   },
   {
-    image: "devices/iphone-SE-mini.jpg",
+    image: "/devices/iphone-SE-mini.jpg",
     name: "Iphone SE Mini",
     price: "120,000",
     rating: 4,
@@ -40,7 +40,7 @@ export const deviceInfo = [
     quantity:0
   },
   {
-    image: "devices/LG_wireless_earbuds.jpg",
+    image: "/devices/LG_wireless_earbuds.jpg",
     name: "lg wireless earbuds",
     price: "45,000",
     category: "accessory",
@@ -48,7 +48,7 @@ export const deviceInfo = [
     quantity:0
   },
   {
-    image: "devices/tecno-camon-20.jpg",
+    image: "/devices/tecno-camon-20.jpg",
     name: "tecno camon 20",
     price: "450,000",
     rating: 4,
@@ -56,7 +56,7 @@ export const deviceInfo = [
     quantity:0
   },
   {
-    image: "devices/oraimo-earpiece.jpg",
+    image: "/devices/oraimo-earpiece.jpg",
     name: "oraimo earpiece",
     price: "8,000",
     rating: 4,
@@ -64,7 +64,7 @@ export const deviceInfo = [
     quantity:0
   },
   {
-    image: "devices/macbook air.jpg",
+    image: "/devices/macbook air.jpg",
     name: "macbook air m1",
     price: "1,599,000",
     rating: 4,
