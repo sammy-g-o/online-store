e2e171017d87590a2ca94766a4c81c99b748ec09
Giwa Abdulsamad <giwaabdulsamad0@gmail.com>
2025-10-30 16:52:19 +0100

    increasing and decreasing product quantity in cart working

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,21 +1,9 @@
 import { useState } from "react";
+import CartItem from "./cartItem";
 
 /* eslint-disable react/prop-types */
-function Cart({cart, onDeleteFromCart}) {
-  
-  const [quantity, setQuantity]=useState();
-  function handleIncreaseQuantity(name) {
-    const filtered = cart.filter((carts)=> carts.name === name)
-    console.log(filtered);
-    
-    const updatedCart = filtered.map((cartItem)=> cart.name === name ?  {...cartItem, quantity: quantity}: cartItem)
-   setQuantity(updatedCart[0].quantity+1)
-   console.log(updatedCart[0].quantity);
-  }
-  function handleDecreaseQuantity() {
-    if(quantity<1)return;
-    setQuantity(quantity-1);
-  }
+function Cart({ cart, onDeleteFromCart }) {
+
   return (
     <>
       <div
@@ -26,27 +14,17 @@ function Cart({cart, onDeleteFromCart}) {
           gap: "20px",
         }}
       >
-        <h2 style={{color:'#081142'}}>Shopping Cart</h2>
+        <h2 style={{ color: '#081142' }}>Shopping Cart</h2>
         {cart.length === 0 && (
           <><div className="empty-cart-message">
-                      <img src="/Cart-Plus--Streamline-Bootstrap (1).svg" alt="" />
-                  </div><h2>Your Cart Is Empty</h2></>
+            <img src="/Cart-Plus--Streamline-Bootstrap (1).svg" alt="" />
+          </div><h2>Your Cart Is Empty</h2></>
         )}
         {cart.length > 0 && (
-            <div className="cart">
+          <div className="cart">
             <div></div>
-            {cart.map((cartItem)=><div className="cart-item" key={crypto.randomUUID()}>
-                <div className="delete-item" onClick={()=>onDeleteFromCart(cartItem.name)}>❌</div>
-                <div><img src={cartItem.image}/></div>
-                <div>{cartItem.name}</div>
-                <div className="quantity">
-                  <button onClick={handleDecreaseQuantity}>-</button>
-                  <p className="show-quantity">{quantity}</p>
-                  <button onClick={()=>handleIncreaseQuantity(cartItem.name)}>+</button>
-                </div>
-                <div>₦{cartItem.price}</div>
-            </div>)}
-            </div>
+            {cart.map((cartItem) => <CartItem cartItem={cartItem} onDeleteFromCart={onDeleteFromCart}/>)}
+          </div>
         )}
       </div>
     </>
---
+++ src/components/cartItem.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+function CartItem({ cartItem, onDeleteFromCart }) {
+    const [quantity, setQuantity] = useState(0);
+    function handleIncreaseQuantity() {
+        setQuantity((quantity) => quantity + 1)
+    }
+    function handleDecreaseQuantity() {
+        if (quantity > 0) {
+            setQuantity(quantity - 1);
+        }
+    }
+    return (<><div className="cart-item" key={crypto.randomUUID()}>
+        <div className="delete-item" onClick={() => onDeleteFromCart(cartItem.name)}>❌</div>
+        <div><img src={cartItem.image} /></div>
+        <div>{cartItem.name}</div>
+        <div className="quantity">
+            <button onClick={handleDecreaseQuantity}>-</button>
+            <p className="show-quantity">{quantity}</p>
+            <button onClick={() => handleIncreaseQuantity(cartItem.name)}>+</button>
+        </div>
+        <div>₦{cartItem.price}</div>
+    </div></>)
+}
+export default CartItem;
