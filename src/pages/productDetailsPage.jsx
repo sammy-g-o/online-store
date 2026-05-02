68b2f2ef6341101537c7bf0e7242fcb7d3be56f2
sammy <142742278+sammy-g-o@users.noreply.github.com>
2026-05-03 00:11:52 +0100

    refactor: simplify ProductDetailsPage by removing unnecessary components

--- src/pages/productDetailsPage.jsx
+++ src/pages/productDetailsPage.jsx
@@ -1,19 +1,11 @@
-import Nav from "../components/nav";
-import Logo from "../components/logo";
-import SearchBar from "../components/searchBar";
-import Icons from "../components/icons";
 import ProductDetails from "../components/productDetails";
-import Footer from "../components/footer";
+
 function ProductDetailsPage(){
 return (
     <>
-      <Nav>
-        <Logo />
-        <SearchBar />
-        <Icons />
-      </Nav>
+      
       <ProductDetails key={crypto.randomUUID()}/>
-      <Footer />
+
     </>
   );
 }
